8c3cd2038e83dee9b133c988c2894dbf8124154d
andreysulymka <andreysulymka5@gmail.com>
2023-11-28 17:06:33 +0200

    add transforming data and component with random character, add spinner and error components

--- marvel-app/src/index.js
+++ marvel-app/src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+
 
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
 
-marvelService
-  .getAllCharacters()
-  .then((res) => res.data.results.forEach((item) => console.log(item.name)));
 
 const root = document.getElementById("root");
 const rootElement = createRoot(root);
