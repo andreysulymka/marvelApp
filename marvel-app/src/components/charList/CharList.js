7994e6f148419bc6d71e3bb027ac1f6829195e11
andreysulymka <andreysulymka5@gmail.com>
2023-11-28 18:56:06 +0200

    change some structure gor CharList component

--- marvel-app/src/components/charList/CharList.js
+++ marvel-app/src/components/charList/CharList.js
@@ -1,40 +1,74 @@
 import { Component } from "react";
 import MarvelService from "../../services/MarvelService";
+import Spinner from "../Spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import "./charList.scss";
-
-
 
 class CharList extends Component {
   state = {
-    list: [],
+    charList: [],
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService();
 
-  updateList = () => {
-    this.marvelService.getAllCharacters().then(this.onListLoad);
-  };
-
-  onListLoad = (arr) => {
-    this.setState({ list: arr });
-  };
-
   componentDidMount() {
-    this.updateList();
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({
+      charList,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
+
+  renderItems(arr) {
+    const items = arr.map((item) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
+
+      return (
+        <li className="char__item" key={item.id}>
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      );
+    });
+
+    return <ul className="char__grid">{items}</ul>;
   }
 
   render() {
-    const { list } = this.state;
+    const { charList, loading, error } = this.state;
+
+    const items = this.renderItems(charList);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
+
     return (
       <div className="char__list">
-        <ul className="char__grid">
-          {list.map((character, index) => (
-            <li key={index} className="char__item">
-              <img src={character.thumbnail} alt={character.name} />
-              <div className="char__name">{character.name}</div>
-            </li>
-          ))}
-        </ul>
+        {errorMessage}
+        {spinner}
+        {content}
         <button className="button button__main button__long">
           <div className="inner">load more</div>
         </button>
